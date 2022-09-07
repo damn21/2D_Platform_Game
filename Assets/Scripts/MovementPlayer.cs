547640ff8fd417aa698e24856276d56d9f85c950
damn21 <dmezae@uninorte.edu.co>
2022-09-07 03:00:53 -0500

    Initial commit

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private bool Grounded;
+    private Animator Animator;
+
+    public float Speed;
+    public float _Jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Animator.SetBool("Running", Horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.5f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.5f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            Jump();
+        }
+        
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * _Jump);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+        
+    }
+}
